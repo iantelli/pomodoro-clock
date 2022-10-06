880c378af8971e38c6d4a3465225f64fe0d36569
Ian Chao <90526260+iantelli@users.noreply.github.com>
2022-10-06 15:23:33 -0700

    changed layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,20 @@
 import * as React from "react";
-import { ChakraProvider, Box, Grid, theme, Heading, Flex, Spacer, VStack } from "@chakra-ui/react";
+import { ChakraProvider, Box, Grid, theme, Button, VStack } from "@chakra-ui/react";
 import { Timer } from "./components/Timer";
 import { Quote } from "./components/Quote";
 import { Navbar } from "./components/Navbar";
 import Axios from "axios";
 
 export const App = () => {
-    const [currentQuote, setCurrentQuote] = React.useState({
-        quote: "Inspirational quotes will appear here as you work",
-        person: "",
-    });
+    const [currentQuote, setCurrentQuote] = React.useState({ quote: "", author: "" });
+
+    const apiCall = async () => {
+        const response = await Axios.get("https://api.quotable.io/random");
+        setCurrentQuote({ quote: response.data.content, author: response.data.author });
+    };
 
     React.useEffect(() => {
-        const int = setInterval(() => {
-            Axios.get("https://motivational-quote-api.herokuapp.com/quotes/random")
-                .then(res => {
-                    setCurrentQuote(res.data);
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        }, 200000);
-        return () => {
-            clearInterval(int);
-        };
+        apiCall();
     }, []);
 
     return (
@@ -32,8 +23,13 @@ export const App = () => {
                 <Navbar />
                 <Grid minH="90vh" p={3}>
                     <VStack spacing="24" justifyContent="center">
-                        <Quote quote={currentQuote.quote} person={currentQuote.person} />
-                        <Timer size="4xl" />
+                        <Box px="16" py="8" borderWidth="3px" borderRadius="lg">
+                            <Timer size="4xl" />
+                        </Box>
+                        <Box>
+                            <Quote quote={currentQuote.quote} author={currentQuote.author} />
+                            <Button onClick={apiCall}>New Quote</Button>
+                        </Box>
                     </VStack>
                 </Grid>
             </Box>
--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,10 +1,10 @@
 import { Box, Heading, Text } from "@chakra-ui/react";
 
-export const Quote = (props: { quote: string; person: string }) => {
+export const Quote = (props: { quote: string; author: string }) => {
     return (
-        <Box>
-            <Heading>"{props.quote}"</Heading>
-            <Text>{props.person}</Text>
+        <Box m="4">
+            <Heading size="lg">"{props.quote}"</Heading>
+            <Text fontStyle="italic">-{props.author}</Text>
         </Box>
     );
 };
